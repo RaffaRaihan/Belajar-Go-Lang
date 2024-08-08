3afd1f82aa07ab336e8725c063151fdf4a7483e9
RaffaRaihan <kaizyexercise@gmail.com>
2024-08-08 09:50:09 +0700

    "Fungsi"

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import "fmt"
+import "strings"
 
 func main() {
-	var pesan string = "Haii"
-
-	fmt.Printf("message: %s \n", pesan)
+	var nama = []string{"Raffa", "Raihan"}
+    printMessage("hallo", nama)
+}
+func printMessage(message string, arr []string) {
+    var nameString = strings.Join(arr, " ")
+    fmt.Println(message, nameString)
 }
