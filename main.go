18ae93401e05a0b800f7b303f9695504c76fa03a
RaffaRaihan <kaizyexercise@gmail.com>
2024-08-08 10:01:01 +0700

    "Fungsi Nilai Balik"

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
-import "strings"
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+var acakan = rand.New(rand.NewSource(time.Now().Unix()))
 
 func main() {
-	var nama = []string{"Raffa", "Raihan"}
-    printMessage("hallo", nama)
+    var isiAcak int
+
+    isiAcak = randomWithRange(2, 10)
+    fmt.Println("Angka acak:", isiAcak)
+
+    isiAcak = randomWithRange(2, 10)
+    fmt.Println("Angka acak:", isiAcak)
+
+    isiAcak = randomWithRange(2, 10)
+    fmt.Println("Angka acak:", isiAcak)
 }
-func printMessage(message string, arr []string) {
-    var nameString = strings.Join(arr, " ")
-    fmt.Println(message, nameString)
+
+func randomWithRange(min, max int) int {
+    var value = acakan.Int()%(max-min+1) + min
+    return value
 }
